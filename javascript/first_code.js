eef922bf746cd94905f3038fac0f4c29283c18be
rose-workneh <144300253+rose-workneh@users.noreply.github.com>
2023-12-23 14:57:03 -0500

    functions

---
+++ javascript/first_code.js
@@ -0,0 +1,257 @@
+//alert ("This is my first code");
+//console.log("welcome to QA class");
+/*instal nodejs
+  first javascript (hello world)
+  introduction part of jav
+  */
+
+ /* console.log("hello tsegereda");
+  console.log(25) 
+  console.log(true)
+  console.log(false)
+  console.log(2+3)
+  console.log(2.51)
+  name = "tsegereda"
+  age = 25
+  //y = true
+  z = false
+  g = 2+3
+
+  console.log(name, age, y, z, g);*/
+
+
+  //1. var
+
+ 
+
+  /*var y = 100
+  var newY = 200
+
+  {
+    var num1 = 100
+
+    console.log(newY)
+  }
+  {
+    var num2 = 200
+    
+    console.log(newY)
+  }
+  {
+    var num3 = 300
+    
+    console.log(newY)
+  }
+num1 = 100
+num2 = 200
+num3 = 300
+
+console.log(num1)
+console.log(num2)
+console.log(num3)*/
+
+/*let data1 = 1000
+let data2 = "hello"
+let data3 = undefined
+let data4 = 2000
+let data5 = 7000
+let data6 = null
+
+let counter = 120
+
+console.log(counter,typeof (data1))
+console.log(data2)
+console.log(data3)
+console.log(data4)
+console.log(data5)
+console.log(data6)*/
+/*var z=4;
+var x=10;
+var y=20;
+
+z=x+y;
+console.log(z);
+x = 10
+console.log(x);
+
+console.log(x)
+
+console.log(x)
+
+x *=5
+console.log(x)
+x /=5
+console.log(x)
+x %=5*/
+//3.oparison oprators//
+
+/*console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+
+
+console.log(true || true)
+console.log(true ||false)
+console.log(false || true)
+console.log(false || false)*/
+
+/*
+if (30 > 4) 
+{
+    console.log("hello world....")
+}*/
+
+/*if(3 > 4)
+{console.log("hello world....")}
+
+else
+
+{console.log("4 > 3")}*/
+ /*mark =75
+
+if (mark  >= 90)
+{
+    Grad = "A";
+    console.log(Grad);
+}
+
+else if (mark >= 80)
+{
+    Grad = "B";
+    console.log(Grad);
+}
+
+else if (mark >= 70)
+{
+    Grad = "C";
+    console.log(Grad);
+}
+
+else if (mark >= 60)
+{
+    Grad ="D";
+    console.log(Grad);
+}
+
+else 
+{
+    
+    console.log(F);
+
+}*/
+
+/*var number = 0;
+if (number == 0)
+{
+    console.log("posetive");
+}
+else if (number < 0)
+{ 
+console.log("Negative");
+}
+else
+{ 
+console.log("Zero");
+}
+
+var number_day = 2;
+if (number_day== 1)
+{
+    console.log("SUNDAY");
+}
+else if(number_day == 2)
+{
+    console.log("Monday");
+}
+else if(number_day == 3)
+{
+    console.log("Tuesday");
+}
+else if(number_day == 4)
+{
+    console.log("Wednesday");
+}
+else if(number_day == 5)
+{
+    console.log("Thursday");
+}
+else if(number_day == 6)
+{
+    console.log("Friday");
+}
+else if(number_day == 7)
+{
+    console.log("Saturday");
+}
+// else{console.log("invalid")}*/
+
+/*var text='number_day';
+switch (Number_day)
+{
+    case 1:
+        console.log("SUNDAY");
+      break;
+    case 2:
+        console.log("Monday");
+      break;
+      case 3:
+        console.log("Tuesday");
+      break;
+    case 4:
+        console.log("Wednesday");
+      break;
+      case 5:
+      console.log("Thursday");
+    break;
+  case 6:
+      console.log("Friday");
+    break;
+    case 7:
+        console.log("Saturday");
+      break;
+      default:
+      text = "Looking forward to the Weekend";
+  }*/
+/*var number = -10;
+if(number % 2 == 0)
+{
+    console.log("even");
+}
+else
+{
+    console.log("odd");
+    
+}
+
+
+result = number < 0 ? "negative" : "positive"
+console.log(result);*/
+
+/*x=1
+do
+{ 
+    console.log("hello world")
+}
+while (x >=10)
+{ 
+    console.log("hello world")
+    x++
+}*/
+  //function
+
+  num1 = 10
+  num2 = 20
+  result = num1 + num2
+  console.log(result)
+
+
+
+
+
+
+
+
+
+
---
+++ javascript/functions.js
@@ -0,0 +1,44 @@
+num1 = 10
+num2 = 20
+result = num1 + num2
+console.log(result)
+
+num1 = 30
+num2 = 40
+result = num1 + num2
+console.log(result)
+
+num1 = 50
+num2 = 60
+result = num1 + num2
+console.log(result)
+
+num1 = 70
+num2 = 80
+result = num1 + num2
+console.log(result)
+
+num1 = 100
+num2 = 200
+result = num1 + num2
+console.log(result)
+
+/*bulit in
+toString()
+Number()
+console.log()
+custom function
+addtwonumber(10,20)*/
+
+
+function addTwonumber(num1,num2)
+{
+    result = num1 + num2
+   console.log(result)
+}
+   
+ addTwonumber(10,20)
+ addTwonumber(30,40)
+ addTwonumber(50,60)
+ addTwonumber(70,80)
+ addTwonumber(100,200)
